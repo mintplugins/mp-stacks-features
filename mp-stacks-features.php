8aca1eb35127e4a0f42600f580b6660dd2642343
Phil Johnston <johnstonphilip@gmail.com>
2014-03-19 23:06:11 -0400

    beta1.0.0.9
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- mp-stacks-features.php
+++ mp-stacks-features.php
@@ -3,7 +3,7 @@
 Plugin Name: MP Stacks + Features
 Plugin URI: http://moveplugins.com
 Description: Content-Type Add-on for MP Stacks which displays "features" about an item in a brick.
-Version: beta1.0.0.7
+Version: beta1.0.0.9
 Author: Move Plugins
 Author URI: http://moveplugins.com
 Text Domain: mp_stacks_features
